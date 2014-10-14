e33839f5c019bb6e4157f6f3984c4253199b280b
camshaft <bytheway.cameron@gmail.com>
2014-10-14 13:52:11 -0600

    augment compiled functions with params iterator

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var plural = require('lang-js-plural');
 var interpolate = require('lang-js-interpolate');
+var reduce = require('directiv-core-reduce');
 
 /**
  * Expose the translate function
@@ -21,7 +22,7 @@ exports = module.exports = translate;
  */
 
 function translate(arr, locale, opts) {
-  if (typeof arr === 'string') return interpolate(arr, opts);
+  if (typeof arr === 'string') return augment(interpolate(arr, opts));
 
   opts = opts || {};
 
@@ -29,20 +30,39 @@ function translate(arr, locale, opts) {
   if (fn.count !== arr.length) throw new Error('missing required length of plural formats: expected ' + fn.count + '; got ' + arr.length);
 
   var fns = [];
-  for (var i = 0, l = arr.length; i < l; i++) {
-    fns.push(interpolate(arr[i], opts));
+  var paramsObj = {};
+  for (var i = 0, l = arr.length, t; i < l; i++) {
+    t = interpolate(arr[i], opts);
+    fns.push(t);
+    merge(paramsObj, t.params);
   }
 
   var key = opts.pluralKey || 'smart_count';
 
-  return function(params) {
+  return augment(function(params) {
     if (typeof params === 'number') params = convert(params, key);
 
     var count = parseInt(params[key], 10);
-    if (Number.isNaN(count)) throw new Error('expected "' + key + '" to be a number. got "' + (typeof params[key]) + '".');
+    if (isNaN(count)) throw new Error('expected "' + key + '" to be a number. got "' + (typeof params[key]) + '".');
 
     return fns[fn(count)](params);
-  };
+  }, Object.keys(paramsObj));
+}
+
+/**
+ * Augment translate functions with params reduce functions
+ *
+ * @param {Function} fn
+ * @param {Array} keys
+ * @return {Function}
+ */
+
+function augment(fn, keys) {
+  keys = keys || fn.params || [];
+  if (!Array.isArray(keys)) keys = Object.keys(keys);
+  fn.params = reduce(keys);
+  fn.params.keys = keys;
+  return fn;
 }
 
 /**
@@ -56,7 +76,7 @@ function lookup(locale) {
   if (!locale) throw new Error('missing required "locale" parameter');
   var fn = plural[locale];
   if (fn) return fn;
-  fn = plural[locale.replace(/_/g, '-').split('-')[0]];
+  fn = plural[locale.split(/[\-_]/)[0]];
   if (fn) return fn;
 
   throw new Error('unsupported locale "' + locale + '"');
@@ -74,4 +94,17 @@ function convert(val, key) {
   var obj = {};
   obj[key] = val;
   return obj;
+}
+
+/**
+ * Merge b into a
+ *
+ * @param {Object} a
+ * @param {Object} b
+ */
+
+function merge(a, b) {
+  for (var key in b) {
+    a[key] = b[key];
+  };
 }
