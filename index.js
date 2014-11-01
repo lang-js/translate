b34bc1ac1a2d45eab137f70c86272ec908a5ff85
camshaft <bytheway.cameron@gmail.com>
2014-10-31 21:48:30 -0600

    add CLDR object support

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
  * Module dependencies
  */
 
-var plural = require('lang-js-plural');
+var plural = {
+  cardinal: require('lang-js-cardinal'),
+  ordinal: require('lang-js-ordinal')
+};
 var interpolate = require('lang-js-interpolate');
 var reduce = require('directiv-core-reduce');
 
@@ -15,39 +18,70 @@ exports = module.exports = translate;
 /**
  * Compile a translation function
  *
- * @param {String|Array} arr
+ * @param {String|Array|Object} cldr
  * @param {String} locale
  * @param {Object?} opts
  * @return {Function}
  */
 
-function translate(arr, locale, opts) {
-  if (typeof arr === 'string') return augment(interpolate(arr, opts));
+function translate(cldr, locale, opts) {
+  if (typeof cldr === 'string') return augment(interpolate(cldr, opts));
 
   opts = opts || {};
 
-  var fn = lookup(locale);
-  if (fn.count !== arr.length) throw new Error('missing required length of plural formats: expected ' + fn.count + '; got ' + arr.length);
+  var pluralize = lookup(locale, cldr._format || opts.pluralFormat);
+  if (Array.isArray(cldr)) cldr = convertArray(cldr, pluralize, opts);
 
-  var fns = [];
+  validate(cldr, pluralize);
+
   var paramsObj = {};
-  for (var i = 0, l = arr.length, t; i < l; i++) {
-    t = interpolate(arr[i], opts);
-    fns.push(t);
-    merge(paramsObj, t.params);
-  }
+  var cases = toFunctions(cldr, pluralize, opts, paramsObj);
 
-  var key = opts.pluralKey || 'smart_count';
+  var key = cldr._plural_key || opts.pluralKey || 'smart_count';
   var validatePluralKey = typeof opts.validatePluralKey === 'undefined' ? true : opts.validatePluralKey;
 
   return augment(function(params) {
-    if (typeof params === 'number') params = convert(params, key);
+    if (typeof params === 'number') params = convertSmartCount(params, key);
 
     var count = parseInt(params[key], 10);
     if (validatePluralKey && isNaN(count)) throw new Error('expected "' + key + '" to be a number. got "' + (typeof params[key]) + '".');
 
-    return fns[fn(count || 0)](params);
+    return (cases[count] || cases[pluralize(count || 0)])(params);
   }, Object.keys(paramsObj));
+}
+
+/**
+ * Validate a cldr against a pluralize function
+ *
+ * @param {Object} cldr
+ * @param {Function} pluralize
+ */
+
+function validate(cldr, pluralize) {
+  pluralize.formats.forEach(function(key) {
+    if (!cldr[key]) throw new Error('translation object missing required key "' + key + '"');
+  });
+}
+
+/**
+ * Convert a cldr object to an object of functions
+ *
+ * @param {Object} cldr
+ * @param {Function} pluralize
+ * @param {Object} opts
+ * @param {Object} paramsObj
+ * @return {Object}
+ */
+
+function toFunctions(cldr, pluralize, opts, paramsObj) {
+  return Object.keys(cldr).reduce(function(acc, key) {
+    if (key.indexOf('_') === 0) return acc;
+    var value = cldr[key];
+    if (typeof value !== 'string') return acc;
+    var t = acc[key] = interpolate(value, opts);
+    merge(paramsObj, t.params);
+    return acc;
+  }, {});
 }
 
 /**
@@ -70,17 +104,39 @@ function augment(fn, keys) {
  * Lookup the plural function given a locale
  *
  * @param {String} locale
+ * @param {String} format
  * @return {Function}
  */
 
-function lookup(locale) {
+function lookup(locale, format) {
   if (!locale) throw new Error('missing required "locale" parameter');
-  var fn = plural[locale];
+  format = format || 'cardinal';
+  var p = plural[format];
+  if (!p) throw new Error('unsupported plural format "' + format + '"');
+  var fn = p[locale];
   if (fn) return fn;
   fn = plural[locale.split(/[\-_]/)[0]];
   if (fn) return fn;
 
   throw new Error('unsupported locale "' + locale + '"');
+}
+
+/**
+ * Convert an array input to a CLDR object
+ *
+ * @param {Array} arr
+ * @param {Function} pluralize
+ * @param {Object} opts
+ * @return {Object}
+ */
+
+function convertArray(arr, pluralize, opts) {
+  if (arr.length !== pluralize.count) throw new Error('missing required length of plural formats: expected ' + pluralize.count + '; got ' + arr.length);
+
+  return pluralize.formats.reduce(function(acc, key, i) {
+    acc[key] = arr[i];
+    return acc;
+  }, {});
 }
 
 /**
@@ -91,7 +147,7 @@ function lookup(locale) {
  * @return {Object}
  */
 
-function convert(val, key) {
+function convertSmartCount(val, key) {
   var obj = {};
   obj[key] = val;
   return obj;
--- test/index.js
+++ test/index.js
@@ -2,7 +2,16 @@ var should = require('should');
 var translate = require('../');
 
 describe('translate', function() {
-  it('should pluralize a phrase', function() {
+
+  it('should support a simple string', function() {
+    translate('Hello!')().should.eql(['Hello!']);
+  });
+
+  it('should support an interpolated string', function() {
+    translate('Hello, %{name}!')({name: 'Joe'}).should.eql(['Hello, ', 'Joe', '!']);
+  });
+
+  it('should pluralize a phrase with an array', function() {
     var fn = translate([
       'a car',
       '%{smart_count} cars'
@@ -11,5 +20,44 @@ describe('translate', function() {
     fn(0).should.eql([0, ' cars']);
     fn(1).should.eql(['a car']);
     fn(2).should.eql([2, ' cars']);
+  });
+
+  it('should pluralize a phrase with a CLDR object', function() {
+    var fn = translate({
+      one: 'a car',
+      other: '%{smart_count} cars'
+    }, 'en');
+
+    fn(0).should.eql([0, ' cars']);
+    fn(1).should.eql(['a car']);
+    fn(2).should.eql([2, ' cars']);
+  });
+
+  it('should pluralize an ordinal phrase with a CLDR object', function() {
+    var fn = translate({
+      _format: 'ordinal',
+      one: '%{smart_count}st car',
+      two: '%{smart_count}nd car',
+      few: '%{smart_count}rd car',
+      other: '%{smart_count}th car'
+    }, 'en');
+
+    fn(0).should.eql([0, 'th car']);
+    fn(1).should.eql([1, 'st car']);
+    fn(2).should.eql([2, 'nd car']);
+    fn(3).should.eql([3, 'rd car']);
+    fn(4).should.eql([4, 'th car']);
+  });
+
+  it('should use a specific number if present', function() {
+    var fn = translate({
+      0: 'There aren\'t any cars!',
+      20: 'Ooo lucky 20!',
+      one: 'a car',
+      other: '%{smart_count} cars'
+    }, 'en');
+
+    fn(0).should.eql(['There aren\'t any cars!']);
+    fn(20).should.eql(['Ooo lucky 20!']);
   });
 });
